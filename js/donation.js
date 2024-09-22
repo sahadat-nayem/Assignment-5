95b53b23853542b4d4494245eceb843b71ca39e3
Sahadat Nayem <sahadatnayem333@gmail.com>
2024-09-23 02:18:05 +0600

    design code added

---
+++ js/donation.js
@@ -0,0 +1,5 @@
+document.getElementById('btn-donate-now').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const addMoney = getInputFirldValueById('input-donate-now');
+})
