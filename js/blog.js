46e3733b060ab032e017fba4bd62924ec2a4db5c
Sahadat Nayem <sahadatnayem333@gmail.com>
2024-09-24 17:00:01 +0600

    more code added

---
+++ js/blog.js
@@ -0,0 +1,5 @@
+const Btn = document.getElementById('show-donation').addEventListener('click', function(){
+    window.location.href = '/index.html';
+  });
+
+  
--- js/donation.js
+++ js/donation.js
@@ -1,29 +1,71 @@
 
 document.getElementById('btn-noakhali-donate-now').addEventListener('click',function(){
     const noakhaliAmount = getInputFirldValueById("input-donate-noakhali")
-    const BalanceAmount = getTextFirldValueById('donation-balance');
+    const BalanceAmount = getTextFirldValueById('donation-balance-noakhali');
     const totalAmount = BalanceAmount + noakhaliAmount;
     const fristBalance = getTextFirldValueById('total-balance');
     const totalBalance = fristBalance  -  totalAmount;
 
-     if(isNaN(noakhaliAmount)){
-        alert('Fild to add money');
+     if(noakhaliAmount <= 0 || isNaN( noakhaliAmount)){
+        alert('Failed to Donation');
         return;
     }
 
-    document.getElementById('donation-balance').innerText = totalAmount;
-    document.getElementById('total-balance').innerText = totalBalance;;
-  
-    if(noakhaliAmount <= 0){
-        alert('File to Donation');
-    }
+    document.getElementById('donation-balance-noakhali').innerText = totalAmount;
+    document.getElementById('total-balance').innerText = totalBalance;
     
     
       
   });
 
+
+document.getElementById('btn-feni-donate-now').addEventListener('click',function(){
+    const noakhaliAmount = getInputFirldValueById("input-donate-feni")
+    const BalanceAmount = getTextFirldValueById('donation-balance-feni');
+    const totalAmount = BalanceAmount + noakhaliAmount;
+    const fristBalance = getTextFirldValueById('total-balance');
+    const totalBalance = fristBalance  -  totalAmount;
+
+     if(noakhaliAmount <= 0 || isNaN( noakhaliAmount)){
+        alert('Failed to Donation');
+        return;
+    }
+
+    document.getElementById('donation-balance-feni').innerText = totalAmount;
+    document.getElementById('total-balance').innerText = totalBalance;
+    
+    
+      
+  });
+
+
+document.getElementById('btn-quota-donate-now').addEventListener('click',function(){
+    const noakhaliAmount = getInputFirldValueById("input-donate-quota")
+    const BalanceAmount = getTextFirldValueById('donation-balance-quota');
+    const totalAmount = BalanceAmount + noakhaliAmount;
+    const fristBalance = getTextFirldValueById('total-balance');
+    const totalBalance = fristBalance  -  totalAmount;
+
+     if(noakhaliAmount <= 0 || isNaN( noakhaliAmount)){
+        alert('Failed to Donation');
+        return;
+    }
+
+    document.getElementById('donation-balance-quota').innerText = totalAmount;
+    document.getElementById('total-balance').innerText = totalBalance;
+    
+    
+      
+  });
+
+
+  const Btn = document.getElementById('blog-btn').addEventListener('click', function(){
+    window.location.href = '/blog.html';
+  });
+
   const historyTab = document.getElementById('show-history');
   const donationTab = document.getElementById('show-donation');
+  const noakhaliForm = document.getElementById('noakhali-form')
   historyTab.addEventListener('click', function(){
     historyTab.classList.add(
         "bg-[#B4F461]"
@@ -32,4 +74,5 @@ document.getElementById('btn-noakhali-donate-now').addEventListener('click',func
     donationTab.classList.remove(
         "bg-[#B4F461]"
     );
+
   });
